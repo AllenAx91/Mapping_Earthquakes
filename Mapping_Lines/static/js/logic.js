bd655ad548484ea3b8672cce98633f1ac8699e6c
AllenAx91 <103393916+AllenAx91@users.noreply.github.com>
2022-07-30 16:42:32 -0400

    13.4.3 Polylines Leaflet

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,13 +2,36 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([37.1975, -97.6664], 5);
+
+// // Coordinates for each point to be used in the line.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+// ];
+
+
+// Coordinates for each point to be used in the polyline.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+let line = [
+  [47.4502, -122.3088],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
+
 
 // Get data from cities.js
 let cityData = cities;
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox/streets-v11',
@@ -19,38 +42,10 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//   console.log(city)
-//   L.marker(city.location)
-//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-// .addTo(map);
-// });
-
-   
-
-// Loop through the cities array and create one circle marker for each city.
-// L.circleMarker([34.0522, -118.2437], {
-//     color: 'black',
-//     fillColor: '#ffffa1',
-//     // fillOpacity: 0.5,
-//     radius: 300
-//  }).addTo(map);
-
-cityData.forEach(function(city) {
-  L.circleMarker(city.location, 
-      {
-        color: 'black',
-        fillColor: '#ffffa1',
-        fillOpacity: 0.5,
-        radius: city.population/100000
-      }
-    )
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-  }
-);
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  dashArray:"10",
+  color: "blue",
+  weight: 4,
+  opacity: 0.5
+}).addTo(map);
